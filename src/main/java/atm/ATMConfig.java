dda6415e54f4a4c272ab6014dac4d93a0e2f529c
thatsawan2002 <thatsawan.c@ku.th>
2022-07-18 18:18:31 +0700

    edited

--- src/main/java/atm/ATMConfig.java
+++ src/main/java/atm/ATMConfig.java
@@ -1,2 +1,24 @@
-package atm;public class ATMConfig {
+package atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfig {
+    @Bean
+    public DataSource dataSource() {
+        return new DataSourceDB();
+    }
+    @Bean
+    public Bank bank(){
+        return  new Bank("My bank", dataSource());
+    }
+    @Bean
+    public  ATM atm(){
+        return new ATM(bank());
+    }
+    @Bean
+    public AtmUI atmUI(){
+        return new AtmUI(atm());
+    }
 }
