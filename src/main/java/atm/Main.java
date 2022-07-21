39b1e9bab2764fef07845ca2b86eed9253234a38
thatsawan2002 <thatsawan.c@ku.th>
2022-07-21 20:30:34 +0700

    edit

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -5,12 +5,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//        DataSourceDB dataSourceDB = new DataSourceDB();
-//        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-//        Bank bank = new Bank("My Bank", dataSourceFile);
-//        ATM atm = new ATM(bank);
-//        AtmUI atmUI = new AtmUI(atm);
-//        atmUI.run();
 
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("bean.xml");
